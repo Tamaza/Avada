058a96fb34e76014a2c35b0bac9f890939b58095
Irakli Grigolia <tazigrigolia@gmail.com>
2020-11-25 13:22:36 -0500

    working on  UI on tamada screen

--- avada/Controllers/TamadaViewController.swift
+++ avada/Controllers/TamadaViewController.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class TamadaViewController : UIViewController{
     
-    @IBOutlet weak var TopView: UIView!
+    @IBOutlet weak var button5: UIButton!
+    
+    @IBOutlet weak var button7: UIButton!
+    
+    @IBOutlet weak var button6: UIButton!
+    
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBOutlet weak var button4: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        TopView.layer.cornerRadius = 5
+        
+        button1.layer.cornerRadius = 10
+        button2.layer.cornerRadius = 10
+        button3.layer.cornerRadius = 10
+        button4.layer.cornerRadius = 10
+        
+        button5.contentVerticalAlignment = .fill
+        button5.contentHorizontalAlignment = .fill
+        button6.contentVerticalAlignment = .fill
+        button6.contentHorizontalAlignment = .fill
+        button7.contentVerticalAlignment = .fill
+        button7.contentHorizontalAlignment = .fill
+        
+        button5.isHighlighted = false
+        
+        //TopView.layer.cornerRadius = 5
         
     }
 }
