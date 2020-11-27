2655eb87e318018094d4e4b6f6a7a754b5c20562
Irakli Grigolia <tazigrigolia@gmail.com>
2020-11-26 22:14:44 -0500

    working on search menu

--- avada/Controllers/SearchViewController.swift
+++ avada/Controllers/SearchViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class SearchViewController : UIViewController {
     
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    var toasts : [Toast] = [
+        Toast(category: "შერეული", name: "ცრემლები", body: "მოდით ამ ჭიქით ხავსმოკიდებულ ეკლესიას,ამ ეკლესიაში დანთებულ სანთელს,ამ სანთლის წინ დაღვრილ ცრემლებს და ამ ცრემლებში განდობილ,საიდუმლოს გაუმარჯოს.")
+    
+    ]
+    
     override func viewDidLoad() {
+        TableView.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
+    
+    
+extension SearchViewController : UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toasts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = TableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = "This is a cell"
+        return cell
+    }
+    
+        
+    
+    
+    
 }
